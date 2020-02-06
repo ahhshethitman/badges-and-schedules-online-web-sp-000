f81df6a2a0c471bc5ff8536164c4d266433b79f3
Hunter Sides <hunter.s.1337@gmail.com>
2020-02-06 18:12:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,14 +14,12 @@ def batch_badge_creator(attendees)
   end
 end
     
+
 def assign_rooms(attendees)
-  puts "Hello,#{name}! You'll be assigned to room #{room}"
-  
-  def assign_rooms(attendees)
   room=0
   attendees.collect do |name|
-    room+=1
-    "Hello, #{name}! You'll be assigned to room #{room}!"
+    room += 1
+    puts "Hello, #{name}! You'll be assigned to room #{room}!"
   end
 end
 
