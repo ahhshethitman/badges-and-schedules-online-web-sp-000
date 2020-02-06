ef317a2e01e4acb222b609a7c7fbf8ec62375304
Hunter Sides <hunter.s.1337@gmail.com>
2020-02-06 18:03:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ def batch_badge_creator(names)
   new_array = []
   
   names.each do |name|
-    new_array << "Hello, my name is #{name}"
+    new_array << "Hello, my name is #{name}."
   
     new_array
   end
