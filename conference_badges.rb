3c0e0917b3eb7d4c326b058f939c4e0db1b4f9fb
Hunter Sides <hunter.s.1337@gmail.com>
2020-02-06 17:51:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,17 @@
 
 badge_maker(name)
   
-  puts "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."
 
 end
 end
+
+  describe '#badge_maker' do
+
+    # Question 1
+
+    it 'should return a formatted badge' do
+      expect(badge_maker(name)).to eq("Hello, my name is #{name}.")
+    end
+
+  end
